3cfbcf3c8596c193595f7ef24506d1bccf45d88f
Brooke Kennison <brookelkennison@gmail.com>
2019-11-04 14:42:34 -0500

    running locally

--- src/main.js
+++ src/main.js
@@ -1,8 +1,47 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuex);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+// new Vue({
+//     // state
+//     data () {
+//         return {
+//             count: 0
+//         };
+//     },
+//     // view
+//     template: `
+//         <div>{{ count }}</div>
+//     `,
+//     // actions
+//     methods: {
+//         increment () {
+//             this.count++;
+//         }
+//     }
+// });
+
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations : {
+        increment (state) {
+            state.count++;
+        }
+    }
+});
+
+store.commit('increment');
+
+/* eslint-disable no-console */
+console.log(store.state.count);
+/* eslint-enable no-console */
